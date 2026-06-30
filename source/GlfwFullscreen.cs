f7519e8d785d06ce988e5521fd15b3be450f2044
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2026-06-30 18:43:06 +0200

    Revert "allow setting icon for GlfwFullscreen"
    This reverts commit 455b2d09120f8171f5515f9edc40e02506be613e.
    Somehow this ended up on the wrong branch.

--- source/GlfwFullscreen.cs
+++ source/GlfwFullscreen.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using ChaosFramework.Graphics.Imaging;
-using ChaosFramework.Math.Vectors;
+using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using TkGlfw = OpenTK.Windowing.GraphicsLibraryFramework;
@@ -11,9 +8,6 @@ namespace ChaosFramework.Platform.Glfw
 {
     public unsafe class GlfwFullscreen : Fullscreen
     {
-        static float SelectX(KeyValuePair<Vector2i, Rgba8Image> v) => v.Key.x;
-        static float SelectY(KeyValuePair<Vector2i, Rgba8Image> v) => v.Key.y;
-
         public readonly NativeWindow window;
 
         public Monitor monitor { get; }
@@ -21,7 +15,7 @@ namespace ChaosFramework.Platform.Glfw
 
         public uint width => monitor.width;
         public uint height => monitor.height;
-        public Vector2i position => monitor.position;
+        public Math.Vectors.Vector2i position => monitor.position;
 
         internal GlfwFullscreen(string title, GlfwMonitor monitor)
         {
@@ -43,30 +37,6 @@ namespace ChaosFramework.Platform.Glfw
         {
             TkGlfw.GLFW.MakeContextCurrent(window.WindowPtr);
             TkGlfw.GLFW.SwapBuffers(window.WindowPtr);
-        }
-
-        void PresentationContext.SetIcon(Icon icon)
-            => SetIcon(icon);
-
-        public void SetIcon(Icon icon)
-        {
-            (Vector2i, RawDataHandle)[] handles = new (Vector2i, RawDataHandle)[icon.imgs.Count];
-            try
-            {
-                int i = 0;
-                foreach(KeyValuePair<Vector2i, Rgba8Image> layer in icon.imgs.OrderBy(SelectY).OrderBy(SelectX))
-                    handles[i++] = (layer.Value.Size(), layer.Value.GetRawData());
-
-                TkGlfw.GLFW.SetWindowIcon(
-                    window.WindowPtr,
-                    handles.Select(img => new TkGlfw.Image(img.Item1.x, img.Item1.y, (byte*)img.Item2.firstElementAddress)).ToArray()
-                    );
-            }
-            finally
-            {
-                foreach((Vector2i, RawDataHandle) handle in handles)
-                    handle.Item2?.Dispose();
-            }
         }
     }
 }
